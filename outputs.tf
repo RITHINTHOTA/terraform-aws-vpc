a55b030ab709f9e0d3360dfbbbc2148c1a53b672
RITHINTHOTA <thotasrisairithin@gmail.com>
2024-06-18 15:55:27 +0530

    commit

--- outputs.tf
+++ outputs.tf
@@ -22,7 +22,7 @@ output "database_subnet_group_id" {
     value = aws_db_subnet_group.default.id
 }
 
-output "database_subnet_group_id" {
+output "database_subnet_group_name" {
     value = aws_db_subnet_group.default.name
 }
 
